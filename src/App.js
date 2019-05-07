dbae3898d708bff28f6bf93d8ab6aa9d5911d255
Sam Parkinson <parkinsonsp42@gmail.com>
2019-05-07 00:48:44 -0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Navbar from './navbar/navbar';
+import STORE from './dummy-store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    state = STORE;
+  }
+
+  render() {
+    return (
+      <div  className='App'>
+        <Navbar />
+        <main>
+          <section>
+            Sidebar
+          </section>
+          <section>
+            Content
+          </section>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/navbar/navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return(
+      <header className="navbar">
+        <h1>
+          <Link to="/">
+            Noteful
+          </Link>
+        </h1>
+      </header>
+    );
+  }
+}
+
+export default Navbar;
